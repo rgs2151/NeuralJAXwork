83fc88827d70cd4767616ca28f715f09911ab5e1
rgs2151 <rgs2151@columbia.edu>
2023-02-24 08:26:23 -0500

    fixed requirements

--- conf.py
+++ conf.py
@@ -1,7 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Documentation build configuration file
 
 import os
 import sys
@@ -10,14 +7,12 @@ sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'NeuralJAXwork'
 copyright = '2023, Rudramani Singha'
 author = 'Rudramani Singha'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc', 
@@ -38,26 +33,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
 
 html_title = "NeuralJAXwork"
 
 html_static_path = []
-# html_theme_options = {
-#     "sidebar_hide_name": False,
-#     "navigation_with_keys": False,
-#     "top_of_page_button": None, #other option = Edit
-# }
-
-
-# # add this at the end of conf.py
-# def run_apidoc(_):
-#     from sphinx.ext.apidoc import main
-#     parent_dir = os.path.dirname(__file__)
-#     module_dir = os.path.join(parent_dir, 'NeuralJAXwork')
-#     main(['-e', '-o', parent_dir, module_dir, '--force'])
-
-# def setup(app):
-#     app.connect('builder-inited', run_apidoc)
